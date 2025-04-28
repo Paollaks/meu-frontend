d2988f1d063057856c73f375fed80190fe0298c6
Paolla Alves <113143021+Paollaks@users.noreply.github.com>
2025-04-27 21:40:03 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Bem-vindo ao Meu FrontEnd!</h1>
+      <p>Vamos construir um site de Recomendação de obras visuais juntos!</p>
+    </div>
+  );
+}
+
+export default App;
